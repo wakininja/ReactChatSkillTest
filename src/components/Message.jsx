45b3ebdb45559b36f1cb554bb17c69eb81915508
Wakil thakur <aryanwakilsamar@gmail.com>
2023-04-01 18:50:06 +0530

    Added responsiveness to the design

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
-      <div className="messsageInfo">
+    <div className='message owner'>
+      <div className="messageInfo">
         <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
         <span>just now</span>
       </div>
-      <div className="messsageContent">
+      <div className="messageContent">
         <p>hello</p>
-        {/* <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" /> */}
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
       </div>
     </div>
   )
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -10,6 +10,10 @@ const Messages = () => {
         <Message/>
         <Message/>
         <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
     </div>
   )
 }
