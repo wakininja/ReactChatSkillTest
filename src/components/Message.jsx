1f3444c6de6c5f4b6f31b319a7045d880467c659
Wakil thakur <aryanwakilsamar@gmail.com>
2023-04-01 18:17:25 +0530

    creating chat component in which input and message part is done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className='message'>
+      <div className="messsageInfo">
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messsageContent">
+        <p>hello</p>
+        {/* <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" /> */}
+      </div>
     </div>
   )
 }
