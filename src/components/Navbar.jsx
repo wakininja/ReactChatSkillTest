d882d2d163a95c4ac3839fac628717b5f9186dee
Wakil thakur <aryanwakilsamar@gmail.com>
2023-04-02 21:49:41 +0530

    added all authentication and redirection for register, Login & home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {signOut} from "firebase/auth"
+import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
-     <span className='logo'>ReactChat</span>
-     <div className="user">
-      <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
-      <span>Wakil</span>
-      <button>Log out</button>
-     </div>
+      <span className="logo">ReactChat</span>
+      <div className="user">
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={()=>signOut(auth)}>logout</button>
+      </div>
     </div>
   )
 }
 
-export default Navbar;
+export default Navbar
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,73 +1,88 @@
+import React, { useState } from "react";
+import Add from "../img/addAvatar.png";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { db,auth,storage } from "../firebase"
+import { auth, db, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import React, { useState } from 'react'
-import { doc, setDoc } from "firebase/firestore"; 
-import Add from "../img/addAvatar.png"
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    setLoading(true);
+    e.preventDefault();
     const name = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
-try{
-  const res = await createUserWithEmailAndPassword(auth, email, password)
+    try {
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-const storageRef = ref(storage, name);
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${name + date}`);
 
-const uploadTask = uploadBytesResumable(storageRef, file);
-uploadTask.on( 
-  (error) => {
-    setErr(true);
-  },   
-  () => {
-    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-      await updateProfile(res.user,{
-        name,
-        photoURL:downloadURL,
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
+            await updateProfile(res.user, {
+              name,
+              photoURL: downloadURL,
+            });
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              name,
+              email,
+              photoURL: downloadURL,
+            });
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+            setLoading(false);
+          }
+        });
       });
-      await setDoc(doc(db, "users", res.user.uid),{
-        uid: res.user.uid,
-        name,
-        email,
-        photoURL: downloadURL,
-      });
-
-      await setDoc(doc(db, "userChats", res.user.uid), {});
-    });
-  }
-);
-
-}catch(err){
-setErr(true);
-}
-  
+    } catch (err) {
+      setErr(true);
+      setLoading(false);
+    }
   };
+
   return (
-    <div className='formContainer'>
-    <div className='formWrapper'>
-    <span className='logo'>ReactChat</span>
-    <span className='title'>Register</span>
-    <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Name' />
-        <input type="email" placeholder='email'/>
-        <input type="password" placeholder='password' />
-        <input style={{display: "none"}} type="file" id='file' />
-        <label htmlFor="file">
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">ReactChat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input required type="text" placeholder="Name" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
             <img src={Add} alt="" />
             <span>Add an avatar</span>
-        </label>
-        <button>Sign up</button>
-        {err && <span>Something went wrong</span>}
-    </form>
-    <p>I have already an account.  LogIn</p>
+          </label>
+          <button disabled={loading}>Sign up</button>
+          {loading && "Uploading and compressing the image please wait..."}
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>
+          You do have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
